4d0a606fc1bd8d95b4bfec9f480cf87e0b8972a0
pravi599 <praveenavallela599@gmail.com>
2024-02-15 08:59:45 +0530

    Last Commit

--- BookStoreSolution/BookStoreApplication/Program.cs
+++ BookStoreSolution/BookStoreApplication/Program.cs
@@ -93,7 +93,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCors("reactApp");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- BookStoreSolution/BookStoreApplication/obj/Debug/net6.0/BookStoreApplication.AssemblyInfo.cs
+++ BookStoreSolution/BookStoreApplication/obj/Debug/net6.0/BookStoreApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f27c953d97bf9cea9b10f7e9d0555be136c3e48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+658d1af528eab4dc0592d7e83ce7885e25d2b301")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
